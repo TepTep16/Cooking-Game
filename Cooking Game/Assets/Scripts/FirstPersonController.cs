43ab1880421052ac35a945ac8e62a68b5187d331
N-bot1 <neomoshoete@gmail.com>
2026-08-22 21:39:48 +0200

    Blockout and menus
    Level graybox and menu

--- Cooking Game/Assets/Scripts/FirstPersonController.cs
+++ Cooking Game/Assets/Scripts/FirstPersonController.cs
@@ -36,6 +36,8 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.timeScale == 0)
+            return;
         HandleMovement();
         HandleRotation();
     }
